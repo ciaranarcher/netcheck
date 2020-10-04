b313deb90c352aba4607dc75d0e8114c992ef33c
Ciaran Archer <carcher@zendesk.com>
2020-10-04 17:23:59 +0100

    Extract stats from ping response.

--- lib/netcheck.rb
+++ lib/netcheck.rb
@@ -3,9 +3,50 @@ module Netcheck
 
   class Check
     def run
-      puts 'starting check'
-
       0 # Successful run
+    end
+  end
+
+  class PingParser
+    attr_reader :packet_loss_percent, :min, :avg, :max, :stddev
+
+    MIN_ROWS       = 6
+    MIN_STATS_ROWS = 2
+    NUM_STATS      = 4
+    NUM_REGEX      = /(\d{1,3}\.\d{1,3})/
+    PKT_LOSS_REGEX = Regexp.compile(NUM_REGEX).freeze
+    STATS_REGEX    = Regexp.compile(([NUM_REGEX.source] * 3).join("\/"))
+
+    def initialize(output)
+      @output = output
+      parse
+    end
+
+    private
+
+    def parse
+      begin
+
+        # First grab the stats rows like so:
+        # 3 packets transmitted, 3 packets received, 0.0% packet loss
+        # round-trip min/avg/max/stddev = 14.886/33.055/66.590/23.740 ms
+
+        rows = @output.split("\n")
+        stat_rows = rows.last(MIN_STATS_ROWS)
+        raise "no stats rows found" if stat_rows.length < MIN_STATS_ROWS
+
+        # Store the packet loss figure
+        @packet_loss_percent = stat_rows[0].match(PKT_LOSS_REGEX)[1]
+
+        # Store the min/avg/max/stddev
+        numbers = stat_rows.last.match(STATS_REGEX)
+        @min    = numbers[1].to_f
+        @avg    = numbers[2].to_f
+        @max    = numbers[3].to_f
+        @stddev = numbers[4].to_f
+      rescue => e
+        raise "error processing ping response: #{e}"
+      end
     end
   end
 end
--- spec/netcheck_spec.rb
+++ spec/netcheck_spec.rb
@@ -1,9 +1,48 @@
-RSpec.describe Netcheck do
-  it "has a version number" do
-    expect(Netcheck::VERSION).not_to be nil
-  end
+module Netcheck
+  RSpec.describe Netcheck do
+    it "has a version number" do
+      expect(VERSION).not_to be nil
+    end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+    it "does something useful" do
+    end
+
+    describe PingParser do
+      let(:non_ping_output) { 'rubbish data' }
+      let(:ping_output) do
+        <<~HEREDOC
+          PING google.ie (209.85.203.94): 56 data bytes
+          64 bytes from 209.85.203.94: icmp_seq=0 ttl=106 time=14.886 ms
+          64 bytes from 209.85.203.94: icmp_seq=1 ttl=106 time=17.690 ms
+          64 bytes from 209.85.203.94: icmp_seq=2 ttl=106 time=66.590 ms
+
+          --- google.ie ping statistics ---
+          3 packets transmitted, 3 packets received, 0.0% packet loss
+          round-trip min/avg/max/stddev = 14.886/33.055/66.590/23.740 ms
+        HEREDOC
+      end
+
+      it 'raises an error when parsing non-ping output' do
+        expect { PingParser.new(non_ping_output) }.to raise_error(RuntimeError, /error processing ping response/)
+      end
+
+      it 'succeeds when parsing ping output' do
+        expect(PingParser.new(ping_output)).to be_instance_of PingParser
+      end
+
+      describe ('reading stats') do
+        let(:parser) {  PingParser.new(ping_output) }
+
+        it 'reads the min value successfully' do
+          expect(parser.min).to equal 14.886
+        end
+        # it 'reads the packet loss percentage successfully' do
+        #   expect(parser.packet_loss_percent).to equal 0.0
+        # end
+        # it 'reads the packet loss percentage successfully' do
+        #   expect(parser.packet_loss_percent).to equal 0.0
+        # end
+      end
+    end
   end
 end
